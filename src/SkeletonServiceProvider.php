9fdb948f6887849ed83753c8c4d631b5de31df79
Brian Dillingham <brian@dillingham.dev>
2021-09-26 18:44:53 -0400

    wip

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -2,24 +2,29 @@
 
 namespace VendorName\Skeleton;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\ServiceProvider;
 use VendorName\Skeleton\Commands\SkeletonCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class SkeletonServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
+    public function register()
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('skeleton')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_skeleton_table')
-            ->hasCommand(SkeletonCommand::class);
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/skeleton.php', 'skeleton'
+        );
+    }
+
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                SkeletonCommand::class,
+            ]);
+        }
     }
 }
